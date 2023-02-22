66b0a0705992b60e4f21570a6e1fedc088a7fd3d
edisaric <edisaric5@gmail.com>
2023-02-22 16:13:22 +0100

    Naredu drugo nalogo

---
+++ naloga_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string ime;
+    cout<<" Enter your name: ";
+    cin>>ime;
+    cout<<"Hi "<<ime<<endl;
+
+     return 0;
+}
