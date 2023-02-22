1e3e38f570cf5f33f7f0f540ccea6b81c279d316
edisaric <edisaric5@gmail.com>
2023-02-22 16:19:45 +0100

    naloga_3.cpp

---
+++ naloga_3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int stevilo;
+    int kvadrat;
+
+    cout<<" Vnesi stevilo: "<<endl;
+    cin>>stevilo;
+    kvadrat=stevilo;
+    cout<<stevilo*kvadrat<<endl;
+
+    return 0;
+}
