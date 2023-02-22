093f8c9c11170d22aa3995a32b8c71f3a1ad0573
edisaric <edisaric5@gmail.com>
2023-02-22 16:34:59 +0100

    naloga_5.cpp

---
+++ naloga_5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string ime;
+    cout<<" Enter your name: ";
+    cin>>ime;
+    cout<<"Hello, "<<ime<<"! "<<endl;
+
+    
+
+    return 0;
+}
